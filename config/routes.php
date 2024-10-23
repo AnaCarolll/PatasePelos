655722a361e2ed62373893dc92736fe6b6d8cbc3
Ana Carolina <anacarolina.silva@onfly.com.br>
2024-10-23 08:26:58 -0300

    modificado a rota update

--- config/routes.php
+++ config/routes.php
@@ -9,13 +9,15 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
+use App\Controller\PetsController;
 use Hyperf\HttpServer\Router\Router;
 
 Router::addGroup('/pet', function (){
     Router::get('','App\Controller\PetsController@list');  //lista todos
     Router::get('/{id}','App\Controller\PetsController@list');  //lista um em especifico
     Router::post('','App\Controller\PetsController@register');
-    Router::put('/{id}','App\Controller\PetsController@update');
+    Router::put('/{id}',[PetsController::class, 'update']);
     Router::delete('/{id}',[\App\Controller\PetsController::class, 'destroy']);
 });
 
